76b560af7365805515073d9cc453e621f9db7bd4
Golikov-Andrey <newsc2@yandex.ru>
2023-12-25 13:41:49 +0300

    solution from seminar 4 OOP StudentApp part2(Generics)

---
+++ OOPSeminar4/java/StudentApp/src/Controllers/AccountController.java
@@ -0,0 +1,19 @@
+package Controllers;
+
+import java.util.List;
+
+import Domain.Person;
+import Domain.Teacher;
+
+public class AccountController {
+
+    public static <T extends Teacher> void paySalary(T person, int salary) {
+        System.out.println(person.getName() + " выплачено зарплата " + salary + "р. ");
+    }
+
+    //Загатовка для метода averageAge
+    public static <T extends Person> double averageAge(List<T> persons)
+    {
+        return 0.0;
+    }
+}
